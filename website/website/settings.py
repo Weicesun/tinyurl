94e53312e9695d78597f8c9219558967401154d9
Weicesun <kevinsun181@gmail.com>
2016-06-09 03:10:13 +0000

    debug postgresql

--- website/website/settings.py
+++ website/website/settings.py
@@ -78,9 +78,9 @@ WSGI_APPLICATION = 'website.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mydatabase',
-        'USER': 'mydatabaseuser',
-        'PASSWORD': 'mypassword',
+        'NAME': 'vagrant',
+        'USER': 'vagrant',
+        'PASSWORD': '1234',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
